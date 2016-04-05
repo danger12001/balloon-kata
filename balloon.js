cf969ae82b58c5819f02f6e4ca01a68c75950457
danger12001 <paul@projectcodex.co>
2016-04-05 10:48:14 +0200

    extra helium price

--- balloon.js
+++ balloon.js
@@ -1,4 +1,51 @@
-exports.processing = function(str){
-var order = str.split(" ");
-console.log(order);
+exports.processingOrders = function(orders){
+var order = orders.replace(/sets of /g,"").replace(/ and /g, "").replace(/, /g, " ").replace(/,/g, " ").split(" ");
+
+var orderMap = [];
+for(x=0;x<order.length;x++){
+  if(x % 2 === 0){
+  var quantity = parseFloat(order[x]) * 3;
+  var color = order[x + 1];
+  orderMap.push({quantity: quantity,
+                balloon: color});
+}
+}
+// console.log(orderMap);
+return orderMap;
+};
+exports.processingPrices = function(prices){
+var price = prices.replace(/ for/g,"").replace(/R/g,"").replace(/ and /g, "").replace(/, /g, " ").replace(/,/g, " ").split(" ");
+var priceMap = [];
+for(x=0;x<price.length;x++){
+  if(x % 2 === 0){
+    var value = parseFloat(price[x]);
+    var colors = price[x + 1];
+    priceMap.push({price: value,
+                  balloon: colors});
+  }
+}
+// console.log(priceMap);
+return priceMap;
+};
+exports.balloonCost = function(orderMap, priceMap){
+  var totalCost = [];
+  for(x=0;x<orderMap.length;x++){
+    var totalPrice = orderMap[x].quantity * priceMap[x].price;
+    var item = orderMap[x].balloon;
+    totalCost.push({totalPrice: totalPrice / 3,
+                    balloon: item});
+  }
+// console.log(totalCost);
+return totalCost;
+};
+exports.heliumCost = function(orderMap){
+var heliumCost = [];
+for(x=0;x<orderMap.length;x++){
+  var helium = orderMap[x].quantity * 2;
+  var items = orderMap[x].balloon;
+  heliumCost.push({helium: helium,
+                  balloon: items});
+}
+// console.log(heliumCost);
+return heliumCost;
 };
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,47 @@
 var assert = require('assert');
 var balloon = require("../balloon");
 
-describe("Balloon",function(){
+describe("processingOrders",function(){
   it("should",function(){
-    var result =  balloon.processing("4 sets of red, 3 sets of blue, and 3 sets of yellow");
-    assert.equal(result,"");
+    var result =  balloon.processingOrders("4 sets of red, 3 sets of blue, and 3 sets of yellow");
+    assert.deepEqual(result,[ { quantity: 12, balloon: 'red' },
+  { quantity: 9, balloon: 'blue' },
+  { quantity: 9, balloon: 'yellow' } ]
+);
+  });
+});
+describe("processingPrices",function(){
+  it("should return a map of the prices for each balloon",function(){
+    var result =  balloon.processingPrices("R4 for red, R5 for blue, and R5.50 for yellow");
+    assert.deepEqual(result,[ { price: 4, balloon: 'red' },
+  { price: 5, balloon: 'blue' },
+  { price: 5.5, balloon: 'yellow' } ]
+);
+  });
+});
+describe("balloonCost",function(){
+  it("should",function(){
+    var result =  balloon.balloonCost([ { quantity: 12, balloon: 'red' },
+  { quantity: 9, balloon: 'blue' },
+  { quantity: 9, balloon: 'yellow' } ],[ { price: 4, balloon: 'red' },
+{ price: 5, balloon: 'blue' },
+{ price: 5.5, balloon: 'yellow' } ]);
+    assert.deepEqual(result,[ { totalPrice: 16, balloon: 'red' },
+  { totalPrice: 15, balloon: 'blue' },
+  { totalPrice: 16.5, balloon: 'yellow' } ]
+);
+  });
+});
+
+describe("heliumCost",function(){
+  it("should",function(){
+    var result =  balloon.heliumCost([ { quantity: 12, balloon: 'red' },
+  { quantity: 9, balloon: 'blue' },
+  { quantity: 9, balloon: 'yellow' } ]);
+    assert.deepEqual(result,[ { helium: 24, balloon: 'red' },
+  { helium: 18, balloon: 'blue' },
+  { helium: 18, balloon: 'yellow' } ]
+
+);
   });
 });
